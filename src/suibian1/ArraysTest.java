4452620b2ac10032feefee93f9157ac9958e2940
kehan <sysukehan@gmail.com>
2016-07-19 14:40:49 +0800

    第四章结束

--- src/suibian1/ArraysTest.java
+++ src/suibian1/ArraysTest.java
@@ -1,7 +1,9 @@
 package suibian1;
 
 import java.util.Arrays;
+import java.util.Spliterator;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
 
 public class ArraysTest {
 	
@@ -14,16 +16,31 @@ public class ArraysTest {
 //		System.out.println(Arrays.toString(arr1));
 		
 		int[] arr2 = new int[]{3, -4, 25, 16, 30, 18};
-		Arrays.parallelPrefix(arr2, new IntBinaryOperator() {
+//		System.out.println(Arrays.toString(arr2));
+		Arrays.parallelPrefix(arr2, 1, 3, new IntBinaryOperator() {
 			
 			@Override
 			public int applyAsInt(int left, int right) {
 				return left * right;
 			}
 		});
-		System.out.println(Arrays.toString(arr2));
+//		System.out.println(Arrays.toString(arr2));
 		
+		int[] arr3 = new int[5];
+//		System.out.println(Arrays.toString(arr3));
+		Arrays.parallelSetAll(arr3, new IntUnaryOperator() {
+
+			@Override
+			public int applyAsInt(int operand) {
+				return operand * 5;
+			}
+			
+		});
+//		System.out.println(Arrays.toString(arr3));
 		
+		int[] arr4 = new int[]{10, 9, 8, 7, 6};
+		Arrays.parallelSort(arr4, 1, 3);
+		System.out.println(Arrays.toString(arr4));
 	}
 
 }
